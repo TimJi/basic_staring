60a2806fc21f06ef8c93438cc0e6596504206717
Tim <timji0210@gmail.com>
2020-04-27 18:13:10 +0800

    Updates

---
+++ precache-manifest.a862abd1a1d20e0e1a62487fd3e5d892.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c94add77fef8fce6bf51b0e61346287",
+    "url": "/basic_staring/index.html"
+  },
+  {
+    "revision": "c22587eb5ecd8951d7f2",
+    "url": "/basic_staring/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "213105b68fc6c3120eb6",
+    "url": "/basic_staring/static/js/2.9857ffd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic_staring/static/js/2.9857ffd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22587eb5ecd8951d7f2",
+    "url": "/basic_staring/static/js/main.9f5e90cf.chunk.js"
+  },
+  {
+    "revision": "207ae1b6f2391fbce345",
+    "url": "/basic_staring/static/js/runtime-main.b2aadd2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basic_staring/static/media/logo.5d5d9eef.svg"
+  }
+]);
